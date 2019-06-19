53f6435e355e30d84690fd1015e4aee14c811420
csilla fulep <fulepcsilla@gmail.com>
2019-06-20 00:45:53 +0200

    predicting with the model

--- projectNo1.py
+++ projectNo1.py
@@ -21,3 +21,13 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']
 
 # Fit the model
 model.fit(X, Y, epochs=150, batch_size=10)
+
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+ # calculate predictions
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
+print(rounded)   
